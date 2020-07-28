33e62de6ebf9a6be45f835ef2c35a45794189b23
Ciprian Turbatu <ciprian@betaroot.app>
2020-07-29 00:21:29 +0300

    add templates for nullable types

--- src/shared/gen.utils/DalUtils.cs
+++ src/shared/gen.utils/DalUtils.cs
@@ -136,6 +136,49 @@ namespace gen.utils
             }
         }
 
+        public static string GetCSharpNullableFriendlyType(string sqlType, string tvpClassName)
+        {
+            switch ((sqlType ?? string.Empty).ToLower())
+            {
+                case "bigint": return "long?";
+                case "binary": return "byte[]";
+                case "bit": return "bool?";
+                case "char": return "string";
+                case "date": return "DateTime?";
+                case "filestream": return "byte[]";
+                case "image": return "byte[]";
+                case "tinyint": return "byte?";
+                case "int": return "int?";
+                case "float": return "double?";
+                case "decimal": return "decimal?";
+                case "money": return "decimal?";
+                case "nchar": return "string";
+                case "ntext": return "string";
+                case "numeric": return "decimal?";
+                case "nvarchar": return "string";
+                case "real": return "Single?";
+                case "rowversion": return "byte[]";
+                case "smalldatetime": return "DateTime?";
+                case "smallint": return "short?";
+                case "smallmoney": return "decimal?";
+                case "sql_variant": return "object";
+                case "text": return "string";
+                case "time": return "TimeSpan?";
+                case "timestamp": return "byte[]";
+
+                case "uniqueidentifier": return "Guid?";
+                case "varbinary": return "byte[]";
+                case "varchar": return "string";
+                case "xml": return "string";
+                case "datetime": return "DateTime?";
+                case "datetime2": return "DateTime?";
+                case "datetimeoffset": return "DateTimeOffset?";
+
+                case "structured": return tvpClassName;
+
+                default: return "object";
+            }
+        }
         public static bool IsDatetime(string sqlType)
         {
             switch ((sqlType ?? string.Empty).ToLower())

16df2ffe063cc243aa4ce54b6a7479ea3b32479c
Ciprian Turbatu <ciprian@betaroot.app>
2020-07-30 17:33:15 +0300

    add nullable support

---
+++ src/shared/collection.extensions/BitArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace collection.extensions
+{
+    public static class BitArrayExtensions
+    {
+        public static byte[] ToBytes(this BitArray bits)
+        {
+            if (bits == null) return null;
+            byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
+            bits.CopyTo(ret, 0);
+            return ret;
+        }
+
+    }
+}

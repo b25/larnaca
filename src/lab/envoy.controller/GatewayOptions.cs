1ef0a6c56a702e882669b975ee07dbeefe0ba574
Kurounin <wiseon3@yahoo.com>
2020-12-27 18:08:50 +0200

    feat: add dockerfile for envoy.gateway and envoy.service.mock

--- src/lab/envoy.controller/GatewayOptions.cs
+++ src/lab/envoy.controller/GatewayOptions.cs
@@ -8,19 +8,19 @@ namespace envoy.controller
         [DataMember(Order = 1)]
         public uint ListenPort { get; set; } = 3000;
 
-        [DataMember(Order = 1)]
+        [DataMember(Order = 2)]
         public uint PerConnectionBufferLimitBytes { get; set; } = 32768; // 32 KiB
 
-        [DataMember(Order = 1)]
+        [DataMember(Order = 3)]
         public uint PeerMaxConcurrentStreams { get; set; } = 100;
 
-        [DataMember(Order = 1)]
+        [DataMember(Order = 4)]
         public uint InitialStreamWindowSize { get; set; } = 65536; // 64 KiB
 
-        [DataMember(Order = 1)]
+        [DataMember(Order = 5)]
         public uint InitialConnectionWindowSize { get; set; } = 1048576; // 1 MiB
 
-        [DataMember(Order = 1)]
+        [DataMember(Order = 6)]
         public uint RouteTimeoutSeconds { get; set; } = 0;
     }
 }
